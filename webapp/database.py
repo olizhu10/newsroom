aa568ad4acb8cbf3ea1b15ed1731445685c4aa6e
ps789 <ps789@cornell.edu>
2019-07-01 15:59:03 -0400

    basic databse information function

--- webapp/database.py
+++ webapp/database.py
@@ -1,1 +1,11 @@
 import sqlite3 as sql
+
+
+def get_cluster(index):
+    db = sqlite3.connect(DATABASE_NAME)
+    c = db.cursor()
+    q = 'SELECT text, summary, title, archive FROM articles WHERE cluster = ?'
+    t = (index, )
+    c.execute(q, t)
+    results = c.fetchall()
+    return results
