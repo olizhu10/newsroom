47665a41989a4e91dbfafed0acb2617fda2601da
ps789 <ps789@cornell.edu>
2019-07-01 13:11:48 -0400

    parallel

--- webapp/init_db.py
+++ webapp/init_db.py
@@ -1,26 +1,35 @@
 import sqlite3
 import sys
 import jsonl
+from multiprocessing import Pool
 
-db = sqlite3.connect('database.db')
-c = db.cursor()
+def identity(x):
+    return x
 
-#Create table ARTICLES
-q = ("CREATE TABLE articles (text STRING, summary STRING, title STRING, archive STRING, cluster INTEGER)")
-c.execute(q)
+def main():
+    db = sqlite3.connect('database.db')
+    c = db.cursor()
 
-#Add articles to database
-with jsonl.open('../clustering/final_clusters.jsonl') as f:
-    clusters = f.read()
-with jsonl.open('../dataset_files/train.jsonl.gz', gzip=True) as ds:
-    articles = ds.read()
-for x in range(len(clusters)):
-    print(x)
-    for article in clusters[x]:
-        for a in articles:
-            if a['archive'] == article:
-                q = "INSERT INTO articles (text, summary, title, archive, cluster) VALUES (?,?,?,?,?)"
-                t = (a['text'], a['summary'], a['title'], a['archive'], x)
-                c.execute(q, t)
-                db.commit()
-                break
+
+    #Create table ARTICLES
+    q = ("CREATE TABLE articles (text STRING, summary STRING, title STRING, archive STRING, cluster INTEGER)")
+    c.execute(q)
+    #Add articles to database
+    with jsonl.open('../clustering/final_clusters.jsonl') as f:
+        clusters = f.read()
+    with jsonl.open('../dataset_files/train.jsonl.gz', gzip=True) as ds:
+        articles = ds.read()
+    with Pool(processes=20) as pool:
+        for x in pool.imap_unordered(identity, range(len(clusters))):
+            print(x)
+            for article in clusters[x]:
+                for a in articles:
+                    if a['archive'] == article:
+                        q = "INSERT INTO articles (text, summary, title, archive, cluster) VALUES (?,?,?,?,?)"
+                        t = (a['text'], a['summary'], a['title'], a['archive'], x)
+                        c.execute(q, t)
+                        db.commit()
+                        break
+
+if __name__ == '__main__':
+    main()
