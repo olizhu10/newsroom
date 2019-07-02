56db6021ef085e7633585b0bd11ad93fe35aac13
Olivia Zhu <olizhu10@gmail.com>
2019-07-02 12:05:09 -0400

    web

--- webapp/app.py
+++ webapp/app.py
@@ -6,20 +6,40 @@ import database as db
 
 app = Flask(__name__, template_folder='templates')
 socketio = SocketIO(app)
+cluster = []
 
 @app.route('/')
 def index():
     return render_template('base.html')
 
-#@socketio.on('cluster id submitted')
-@app.route('/cluster', methods=['POST'])
+@app.route('/cluster', methods=['POST', 'GET'])
 def get_cluster():
     if request.method == 'POST':
-        #cluster_id = json[cluster_id]
         cluster_id = request.form['cluster']
-        print(cluster_id)
         cluster = db.get_articles(cluster_id)
+        json = { cluster: cluster}
+        socketio.emit('cluster retrieved', cluster)
         return render_template('cluster.html', cluster=cluster)
+
+@app.route('/article', methods=['POST'])
+def get_article():
+    if request.method == 'POST':
+        text = request.form['article-submit']
+        json = {'text': text}
+    socketio.emit('article selected', json)
+    return render_template('article.html', article=text)
+
+@app.route('/summary', methods=['POST', 'GET'])
+def get_summary():
+    if request.method == 'POST':
+        index = int(request.form['summary-submit'])
+        f = request.form
+        for key in f.keys():
+            for value in f.getlist(key):
+                print(key,":",value)
+        print(cluster)
+        text = cluster[index]
+    return render_template('summary.html', cluster=cluster, summary=text, article=text)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- webapp/socket.js
+++ webapp/socket.js
@@ -1,25 +1,55 @@
-var socket = io.connect('http://' + document.domain + ':' + location.port);
+$(document).ready(function() {
+  var socket = io.connect('http://' + document.domain + ':' + location.port);
+  var cluster = []
 
-$('form#cluster').on('submit', function(e)) {
+  $(document).on('submit', 'form#cluster', function(e) {
+      e.preventDefault()
+      let cluster_id = $('input#cluster').val()
+      socket.emit('cluster id submitted', {
+        cluster_id : cluster_id
+      });
+      console.log(cluster_id)
+      $( 'input#cluster' ).val('').focus()
+    } );
+
+  socket.on('cluster received', function(json) {
+    let cluster = json['cluster']
+  })
+
+  $(document).on('submit', 'form#summary-form', function(e) {
     e.preventDefault()
-    let cluster_id = $('input#cluster').val()
-    socket.emit('cluster id submitted', {
-      cluster_id : cluster_id
-    })
-    $( 'input#cluster' ).val('').focus()
-  } )
+    let index = $('select#summary-select').val()
+    $('div#summary')
+  })
 
+  $('#summary').on('submit', function(e) {
+    e.preventDefault()
+    let num = $('input#summary_submit').val()
+    $('div.summaries').hide()
+    $('div#num.summaries').show()
+  });
 
-$('#summary').on('submit', function(e)) {
-  e.preventDefault()
-  let num = $('input#summary_submit').val()
-  $('div.summaries').hide()
-  $('div#num.summaries').show()
-}
+  $('#article').on('submit', function(e) {
+    e.preventDefault()
+    let num = $('input#article_submit').val()
+    $('div.articles').hide()
+    $('div#num.articles').show()
+  });
 
-$('#article').on('submit', function(e)) {
-  e.preventDefault()
-  let num = $('input#article_submit').val()
-  $('div.articles').hide()
-  $('div#num.articles').show()
-}
+  socket.on('article selected', function(json) {
+    var p = document.createElement('p')
+    var text = document.createTextNode(json['text'])
+    p.appendChild(text)
+    var div = document.getElementbyId('article')
+    div.appendChild(p)
+  });
+
+  socket.on('summary selected', function(json) {
+    var p = document.createElement('p')
+    var text = document.createTextNode(json['text'])
+    p.appendChild(text)
+    var div = document.getElementbyId('summary')
+    div.appendChild(p)
+  });
+
+});
