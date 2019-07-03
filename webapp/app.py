5bf313260390fae900bd55c8fcd0e9c0cd509d11
Olivia Zhu <olizhu10@gmail.com>
2019-07-03 14:59:08 -0400

    webapp

--- webapp/app.py
+++ webapp/app.py
@@ -3,6 +3,8 @@ from flask_socketio import SocketIO
 import eventlet
 import sqlite3
 import database as db
+from fragments import Fragments
+import os
 
 app = Flask(__name__, template_folder='templates')
 socketio = SocketIO(app)
@@ -10,7 +12,12 @@ cluster = []
 
 @app.route('/')
 def index():
-    return render_template('base.html')
+    return render_template('base.html', last_updated=dir_last_updated('static'))
+
+def dir_last_updated(folder):
+    return str(max(os.path.getmtime(os.path.join(root_path, f))
+               for root_path, dirs, files in os.walk(folder)
+               for f in files))
 
 @app.route('/cluster', methods=['POST', 'GET'])
 def get_cluster():
@@ -18,15 +25,51 @@ def get_cluster():
         cluster_id = request.form['cluster']
         global cluster
         cluster = db.get_articles(cluster_id)
-        return render_template('cluster.html', cluster=cluster)
+        return render_template('cluster.html', cluster=cluster, last_updated=dir_last_updated('static'))
 
-@socketio.on('send summary cluster')
-def send_cluster():
-    socketio.emit('summary cluster retrieved', cluster)
+@socketio.on('send cluster')
+def send_summary_cluster():
+    socketio.emit('cluster retrieved', cluster)
 
-@socketio.on('send article cluster')
-def send_cluster():
-    socketio.emit('article cluster retrieved', cluster)
+@socketio.on('send info')
+def send_info(json):
+    try:
+        summary = json['summary']
+        article = json['article']
+        print(json)
+        global cluster
+        fragments = Fragments(cluster[int(summary)][1], cluster[int(article)][0])
+        json = {'density': fragments.density(),
+                'coverage': fragments.coverage(),
+                'compression': fragments.compression()}
+        socketio.emit('info sent', json)
+    except:
+        pass
 
+@socketio.on('create plot')
+def create_plot():
+    colors = ['red','blue','pink','yellow','black','orange','purple','green','cyan',
+    'magenta','grey']
+    plt.title(event)
+    plt.xlabel('coverage')
+    plt.ylabel('density')
+
+    coverages = []
+    densities = []
+    for x in range(len(cluster)):
+        title = cluster[x][2]
+    
+    for x in range(len(matrix)):
+        coverages = []
+        densities = []
+        title = matrix[x][0].getTitle()[:20]
+        for obj in matrix[x]:
+            if obj.getMatch() == True:
+                coverages.append(obj.getCoverage())
+                densities.append(obj.getDensity())
+        plt.scatter(coverages, densities, marker = 'o', c=colors[x], label=title, alpha=0.6)
+    plt.legend()
+    plt.savefig('../events/data/'+event+'.png')
+    plt.show()
 if __name__ == '__main__':
     socketio.run(app, debug=True)
---
+++ webapp/static/socket.js
@@ -0,0 +1,61 @@
+var socket = io.connect('http://' + document.domain + ':' + location.port);
+var cluster;
+var summary;
+var article;
+
+$(document).on('submit', 'form#select-form', function(e) {
+  e.preventDefault();
+  socket.emit('send cluster');
+});
+
+$(document).on('submit', 'input#input-button', function(e){
+  e.preventDefault();
+  json = {'summary':summary,
+          'article':article}
+  socket.emit('create plot', json)
+});
+
+socket.on('cluster retrieved', function(msg) {
+  cluster = msg;
+  show_summary(cluster);
+  show_article(cluster);
+  if (summary===undefined || article===undefined) {}
+  else {
+    json = {'summary':summary,
+            'article':article}
+    console.log(json)
+    socket.emit('send info', json);
+  }
+});
+
+socket.on('info sent', function(json) {
+  let density = json['density'];
+  let coverage = json['coverage'];
+  let compression = json['compression']
+
+  update_info(density, coverage, compression);
+})
+
+function show_summary(cluster) {
+  let index = $('select#summary-select').val();
+  summary = index
+  let summary_text = document.getElementById('summary-text');
+  summary_text.innerHTML = cluster[index][1];
+}
+
+function show_article(cluster) {
+  let index = $('select#article-select').val();
+  article = index
+  let article_text = document.getElementById('article-text');
+  article_text.innerHTML = cluster[index][0];
+}
+
+function update_info(density, coverage, compression) {
+  var pcov = document.getElementById('coverage')
+  var pdens = document.getElementById('density')
+  var pcomp = document.getElementById('compression')
+
+  pcov.innerHTML = "Coverage: "+coverage;
+  pdens.innerHTML = "Density: "+density;
+  pcomp.innerHTML = "Compression: "+compression;
+}
