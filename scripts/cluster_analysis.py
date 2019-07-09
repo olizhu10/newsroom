5306f3c08c4b2575a762274797c2a37e1924a98a
Olivia Zhu <olizhu10@gmail.com>
2019-07-09 13:17:09 -0400

    cluster analysis

---
+++ scripts/cluster_analysis.py
@@ -0,0 +1,49 @@
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot(array, title):
+    print('plotting histogram for '+title)
+    print('close plot to see stats')
+    plt.title(title)
+    plt.xlabel('match percentages')
+    plt.ylabel('number of clusters')
+    ticks = [0,10,20,30,40,50,60,70,80,90,100]
+    plt.xticks(ticks)
+    s = ('total number of clusters: '+str(len(total_summaries))+
+        '\nnumber of valid clusters: '+str(good_clusters)+
+        '\naverage number of summaries per cluster: '+str(avg_num_summaries)+
+        '\nmean: '+str(avg_match_percent)+
+        '\nmedian: '+str(np.median(match_percents))+
+        '\nstd dev: '+str(np.std(match_percents)))
+    plt.text(-4,7,s)
+    plt.hist(array, bins=ticks)
+    plt.savefig(title)
+    plt.show()
+
+if __name__ == '__main__':
+    with open('../clustering/sample_cluster_analysis.csv') as f:
+        reader = csv.DictReader(f)
+
+        total_summaries = []
+        match_percents = []
+        good_clusters = 0
+
+        for row in reader:
+            total_summaries.append(row['num_summaries'])
+            if row['quit'] != 'Y':
+                match_percents.append(row['match_percents'])
+            if row['clustered_yn'] == 'yes':
+                good_clusters += 1
+        match_percents = np.array(match_percents).astype(np.float)
+        total_summaries = np.array(total_summaries).astype(np.float)
+
+        avg_match_percent = np.mean(match_percents)
+        avg_num_summaries = np.mean(total_summaries)
+
+        print('total number of clusters: '+str(len(total_summaries)))
+        print('number of valid clusters: '+str(good_clusters))
+        print('average number of summaries per cluster: '+str(avg_num_summaries))
+        plot(match_percents, 'match percentages')
+        print('mean: '+str(avg_match_percent)+'\nmedian: '+str(np.median(match_percents))+
+            '\nstd dev: '+str(np.std(match_percents)))
