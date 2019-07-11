b93bf8cd2723ec2d47ae89b5b9da064a40ae7171
ps789 <ps789@cornell.edu>
2019-07-11 16:54:00 -0400

    faster init db

--- webapp/init_db.py
+++ webapp/init_db.py
@@ -13,33 +13,32 @@ with jsonl.open('../clustering/final_clusters_0.9_cleaned.jsonl') as f:
 with jsonl.open('../dataset_files/train.jsonl.gz', gzip=True) as ds:
     articles = ds.read()
 
-def identity(x):
-    return x
-
-def addArticle(x):
-    tList = []
-    for article in clusters[x]:
-        for a in articles:
-            if a['archive'] == article:
-                t = (a['text'], a['summary'], a['title'], a['archive'], x)
-                tList.append(t)
-                break
-    return tList
+def createDictionary():
+    dict = {}
+    pbar = tqdm(total=len(clusters), desc='Generating Dictionary:')
+    for clusterInd in range(len(clusters)):
+        for article in clusters[clusterInd]:
+            if article in dict:
+                dict[article].append(clusterInd)
+            else:
+                dict[article] = [clusterInd]
+        pbar.update(1)
+    return dict
 
 def main():
-
     #Create table ARTICLES
     q = ("CREATE TABLE articles (text STRING, summary STRING, title STRING, archive STRING, cluster INTEGER)")
     c.execute(q)
     #Add articles to database
-    pbar = tqdm(total=len(clusters), desc='Generating Database:')
-    with Pool(processes=17) as pool:
-        for tList in pool.imap_unordered(addArticle, range(len(clusters))):
-            q = "INSERT INTO articles (text, summary, title, archive, cluster) VALUES (?,?,?,?,?)"
-            for t in tList:
-                c.execute(q, t)
+    dict = createDictionary()
+    pbar = tqdm(total=len(articles), desc='Parsing json:')
+    for article in articles:
+        q = "INSERT INTO articles (text, summary, title, archive, cluster) VALUES (?,?,?,?,?)"
+        if(article in dict):
+            for t in dict[article]:
+                c.execute(q, (article['text'], article['summary'], article['title'], article['archive'], t))
                 db.commit()
-            pbar.update(1)
+        pbar.update(1)
 
 
 if __name__ == '__main__':
