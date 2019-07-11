58b0b7ac737e4162207685ef55b42b86b227f909
ps789 <ps789@cornell.edu>
2019-07-11 16:13:35 -0400

    clean_db slow?

---
+++ webapp/clean_db.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+def clean_articles(cluster_id):
+    db = sqlite3.connect('database.db')
+    c = db.cursor()
+    q = "SELECT text, archive FROM articles WHERE cluster=?"
+    t = (cluster_id,)
+    c.execute(q,t)
+    articles = c.fetchall()
+    text = ""
+    count = 0
+    for a in articles:
+        if (text != a[0]):
+            text = a[0]
+            q = "DELETE FROM articles WHERE archive = ? AND cluster=?"
+            t = (a[1], cluster_id)
+            c.execute(q,t)
+        else:
+            count += 1
+    if count <= 3:
+        q = "DELETE FROM articles WHERE cluster=?"
+        t = (cluster_id,)
+        c.execute(q,t)
+
+
+if __name__ == '__main__':
+    for i in range(15742):
+        clean_articles(i)
+        print(i)
