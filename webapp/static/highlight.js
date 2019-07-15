076da770a7a557c4927a9db24cf6bd3e4d860d5d
Olivia Zhu <olizhu10@gmail.com>
2019-07-15 11:24:57 -0400

    bugs kinda fixed

--- webapp/static/highlight.js
+++ webapp/static/highlight.js
@@ -9,16 +9,51 @@ function htmlDecode(input){
   return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
 }
 
-function highlight(keyword, i) {
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+}
+
+function highlight2(keyword, i) {
   var article = document.querySelector("#article-text");
   var summary = document.querySelector("#summary-text");
 
-  var exp = htmlDecode(keyword)
-  var re = new RegExp(exp, 'g')
+  var exp = htmlDecode(keyword);
+  var esc = escapeRegExp(exp);
+  var re = new RegExp(esc, 'g');
+  console.log(exp);
+  console.log(esc);
+  console.log(re);
 
-  article.innerHTML = article.innerHTML.replace(re, '<span class="frag'+i+'">'+exp+'</span>')
-  summary.innerHTML = summary.innerHTML.replace(re, '<span class="frag'+i+'">'+exp+'</span>')
+  article.innerHTML = article.innerHTML.replace(re, '<span class="frag'+i+'">'+exp+'</span>');
+  summary.innerHTML = summary.innerHTML.replace(re, '<span class="frag'+i+'">'+exp+'</span>');
 
+  var elements = document.querySelectorAll('.frag'+i);
+  for (let x=0; x<elements.length; x++) {
+    elements[x].style.backgroundColor = colors[i]
+  }
+}
+
+function highlight(keyword,i) {
+  var article = document.querySelector("#article-text");
+  var summary = document.querySelector("#summary-text");
+
+  var exp = htmlDecode(keyword);
+  var esc = escapeRegExp(exp);
+  var re = new RegExp(esc, 'g');
+
+  var ainstance = new Mark(article);
+  var sinstance = new Mark(summary);
+
+  ainstance.mark(keyword, {
+    className: 'frag'+i,
+    separateWordSearch: false,
+    }
+  );
+  sinstance.mark(keyword, {
+    className: 'frag'+i,
+    separateWordSearch: false,
+    }
+  );
   var elements = document.querySelectorAll('.frag'+i);
   for (let x=0; x<elements.length; x++) {
     elements[x].style.backgroundColor = colors[i]
